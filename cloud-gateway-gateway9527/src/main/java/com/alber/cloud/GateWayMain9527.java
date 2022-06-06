9d5c4f6520d58caa782ab3852d83ab3a48b61ffb
albert.yang <18438049166@163.com>
2022-06-07 00:10:32 +0800

    提交gateway的练习

---
+++ cloud-gateway-gateway9527/src/main/java/com/alber/cloud/GateWayMain9527.java
@@ -0,0 +1,19 @@
+package com.alber.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author yjw
+ * @date 2022/6/6 23:34
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class);
+    }
+
+}
---
+++ cloud-gateway-gateway9527/src/main/java/com/alber/cloud/config/GateWayConfig.java
@@ -0,0 +1,20 @@
+package com.alber.cloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yjw
+ * @date 2022/6/6 23:56
+ */
+@Configuration
+public class GateWayConfig {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes().route("path_route", r -> r.path("/guonei").uri("https://news.baidu.com")).build();
+    }
+
+}
