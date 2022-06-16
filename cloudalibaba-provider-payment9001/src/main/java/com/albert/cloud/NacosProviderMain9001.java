4bc045f3e45f34160015efef3d086c402e13cf74
yangjunwei <18438049166@163.com>
2022-06-16 21:01:29 +0800

    nacos

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/albert/cloud/NacosProviderMain9001.java
@@ -0,0 +1,19 @@
+package com.albert.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author yangjunwei
+ * @date 2022/6/16 8:56 下午
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosProviderMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderMain9001.class);
+    }
+
+}
